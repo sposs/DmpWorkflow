96894e736f81747acb49b81210f73e3db8e73cac
Stephane Poss <stephposs@gmail.com>
2016-03-15 23:41:41 +0100

    use requests

--- scripts/dampe-cli-fetch-new-jobs.py
+++ scripts/dampe-cli-fetch-new-jobs.py
@@ -4,20 +4,19 @@ Created on Mar 15, 2016
 @author: zimmer
 '''
 import copy
-from core import db
-import core.models as models
 from core.DmpJob import DmpJob
-
+import requests
 if __name__ == "__main__":
     newJobInstances = []
-    db.connect() # connect to DB
-    for job in models.Job.objects:
-        newJobs = [j for j in job.jobInstances if j.status == 'New']
+    res = requests.get("some_url_to_define/jobs")
+    res.raise_for_status()
+    for job in res.json():
+        newJobs = [j for j in job['jobInstances'] if j['status'] == 'New']
         if len(newJobs):
-            dJob = DmpJob(job.body)
+            dJob = DmpJob(job['body'])
             for j in newJobs:
                 dInstance = copy.deepcopy(dJob)
-                dInstance.setInstanceParameters(j.body)
+                dInstance.setInstanceParameters(j['body'])
                 newJobInstances.append(dInstance)
     print 'found %i new job instances to deploy'%len(newJobInstances)
     
