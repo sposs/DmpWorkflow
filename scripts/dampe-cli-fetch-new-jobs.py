f1494d4b66b60840f956230b0682909195df3dfd
Stephane Poss <stephposs@gmail.com>
2016-03-15 23:58:22 +0100

    better code

--- scripts/dampe-cli-fetch-new-jobs.py
+++ scripts/dampe-cli-fetch-new-jobs.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     newJobInstances = []
     res = requests.get("some_url_to_define/jobs")
     res.raise_for_status()
-    for job in res.json():
-        newJobs = [j for j in job['jobInstances'] if j['status'] == 'New']
+    for job in res.json().items():
+        newJobs = [j for j in job["jobInstances"] if j['status'] == 'New']
         if len(newJobs):
             dJob = DmpJob(job['body'])
             for j in newJobs:
